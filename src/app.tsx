b9dd9eb103f432c3275f3466b13941b614343f8d
chenyansong <chenyansong.cys@bytedance.com>
2025-04-25 11:15:32 +0800

    feat: tailwind test

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import './app.css';
 
 const App = () => {
   const [num, setNum] = useState(0);
   return (
-    <>
-      <h1>{num}</h1>
+    <div className="app">
+      <h1 className="text-3xl font-bold underline">{num}</h1>
       <button onClick={() => setNum(num + 1)}>add one</button>
-    </>
+      <br />
+      <h2 className="text-dark bg-gold">tailwind css</h2>
+      <h2 className="text-dark bg-red-50">bg-red-50</h2>
+    </div>
   );
 };
 
